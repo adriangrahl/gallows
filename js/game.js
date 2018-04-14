c4dbea12a89d0d488ab6e58df421a6ab6ec7513c
adriangrahl <adrian.grahl@gmail.com>
2018-04-14 16:36:19 -0300

    Game inner logic, processing an input

--- js/game.js
+++ js/game.js
@@ -1,4 +1,31 @@
-var createGame = function () {
+var createGame = function (sprite) {
+
+  var findChar = function (char, startFrom) {
+    return secretWord
+      .split('')
+      .indexOf(char, startFrom + 1);
+  };
+
+  var processInput = function(char) {
+    var idxFound = secretWord
+      .split('')
+      .filter(c => c.includes(char))
+      .reduce((currentIdx, c, i, arr) => {
+        var result = findChar(c, currentIdx);
+        fillGap(c, result);
+        return result;
+      },-1);
+
+    if (idxFound > -1) {
+      console.log('You nailed it!');
+      return getGaps();
+    } else
+      sprite.nextFrame();
+  };
+
+  var fillGap = function (char, pos) {
+    letterList[pos] = char;
+  };
 
   var createGaps = function () {
     letterList = Array(secretWord.length).fill('');
@@ -29,6 +56,7 @@ var createGame = function () {
   return {
     setSecretWord : setSecretWord,
     getStage : getStage,
-    getGaps : getGaps
+    getGaps : getGaps,
+    processInput : processInput
   }
 };
